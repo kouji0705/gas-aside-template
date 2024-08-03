216af196321795916af8596dda63cd2dc5ea5a1c
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2024-08-03 11:12:42 +0900

    add: get

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,24 @@ function doPost(
 
 	// ペイロードを解析（必要に応じて）
 	const data = JSON.parse(payload);
-
+	console.log("=======data: ", data);
 	const spreadsheetUtil = new SpreadsheetUtil();
 	const a1cell = spreadsheetUtil.getCellValue("Sheet1", "A1");
-	console.log('=======HIT8 ', a1cell);
+	console.log("a1cell", a1cell);
 	return ContentService.createTextOutput(`Received payload: ${payload}`);
+}
+
+function doGet(
+	e: GoogleAppsScript.Events.DoPost,
+): GoogleAppsScript.Content.TextOutput {
+	// POSTリクエストのペイロードを取得
+	const payload = e.postData.contents;
+
+	// ペイロードを解析（必要に応じて）
+	const data = JSON.parse(payload);
+	console.log("=======data: ", data);
+	const spreadsheetUtil = new SpreadsheetUtil();
+	const a1cell = spreadsheetUtil.getCellValue("Sheet1", "A1");
+	console.log("a1cell", a1cell);
+	return ContentService.createTextOutput(`Received payload: ${a1cell}`);
 }
